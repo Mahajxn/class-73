ff2eaf5c394e148a137d3125400fe7089c1aa294
Mahajxn <71448569+Mahajxn@users.noreply.github.com>
2021-06-23 14:15:32 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,28 @@
+// import * as firebase from 'firebase'
+
+// require('@firebase/firestore')
+//  var firebaseConfig = {
+//     apiKey: "AIzaSyA9P37VdqsJ4uMQb283F2QmIXFyHoxh-XM",
+//    authDomain: "willy-a9800.firebaseapp.com",
+// databaseURL: "https://willy-a9800.firebaseio.com",
+//    projectId: "willy-a9800",
+//     storageBucket: "willy-a9800.appspot.com",
+//     messagingSenderId: "896521218739",
+//      appId: "1:896521218739:web:fcff2b37e49af58db29ec6"
+//    }; 
+//      // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig)
+
+// // var firebaseConfig = {
+// //     apiKey: "",
+// //     authDomain: "wily-app-86cec.firebaseapp.com",
+// //     databaseURL: "https://wily-app-86cec.firebaseio.com",
+// //     projectId: "wily-app-86cec",
+// //     storageBucket: "wily-app-86cec.appspot.com",
+// //     messagingSenderId: "436866087439",
+// //     appId: "1:436866087439:web:1a3bcf134182eb6559017a"
+// //   };
+// //   // Initialize Firebase
+// //   firebase.initializeApp(firebaseConfig);
+// const db=firebase.firestore()
+//   export default db ;
